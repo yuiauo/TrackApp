f2a1199e6a101ed16faa0996e1abe8b6ea109fce
Yuri Bakurov <yuri.bakurov@arsis.ru>
2024-08-31 23:18:00 +0300

    feat: added postgres environment

--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
-    ...
+class DbSettings(BaseSettings):
+
+    PORT: str
+    HOST: str
+    DATABASE: str
+    USER: str
+    PASSWORD: str
+
+    class Config:
+        env_file = ".env"
 
